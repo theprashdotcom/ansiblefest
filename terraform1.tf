30667a55042411bfb309dd5ac6e87c0377697d28
prasanthkp89 <prasanthkp89@outlook.com>
2026-03-24 20:04:44 -0400

    cli commit

--- terraform1.tf
+++ terraform1.tf
@@ -1,2 +1,16 @@
 
-this is added to show git ops
+
+this is my file nd m wis to do what ever i want.
+
+this is my eseodnd commi and i dont tyoe corrct i knew i 
+
+
+d
+fd
+d
+gs
+dG
+sGsG
+sasderhtsstnsnn
+
+
